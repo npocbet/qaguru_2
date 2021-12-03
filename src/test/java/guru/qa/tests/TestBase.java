3a86b2aeb9ddae19d01be7088c0b1d7763bc296a
cbet <npocbet@gmail.com>
2021-12-03 08:15:29 +0300

    local fixes

--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -1,13 +1,17 @@
 package guru.qa.tests;
 
 import com.codeborne.selenide.Configuration;
+import guru.qa.pages.AutomationPracticePage;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
+    AutomationPracticePage automationPP = new AutomationPracticePage();
+
     @BeforeAll
     static void setup(){
+
         Configuration.remote = "https://user1:123@selenoid.autotests.cloud/wd/hub/";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
